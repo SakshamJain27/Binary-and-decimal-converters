7f288b1e1b53c9636a23708513c21dec656878b1
Saksham Jain <106463730+SakshamJain27@users.noreply.github.com>
2022-05-30 20:34:22 +0530

    Create With Module.py

---
+++ With Module.py
@@ -0,0 +1,10 @@
+#pip install binaryformation
+
+#import the module
+import binaryformation as convert
+
+#I am using 12345, you can use any decimal number
+convert.convert_Decimal_To_binary(12345)
+
+#I am using 10001, you can use any binary number
+convert.convert_Binary_To_decimal(10001)
